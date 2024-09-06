371e75693159929df36f07107a9e78949f007aac
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-09-06 14:45:53 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./redux/store.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="product-list-app">
         <App />
       </BrowserRouter>
     </Provider>
