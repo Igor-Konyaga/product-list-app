f2091709f3271931355945feb7b3688ef2ccd0b3
Igor-Konyaga <99217226+Igor-Konyaga@users.noreply.github.com>
2024-09-10 20:08:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./redux/store.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='skill-sniffer.online'>
+      <BrowserRouter basename='/'>
         <App />
       </BrowserRouter>
     </Provider>
